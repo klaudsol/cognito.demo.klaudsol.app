dc52c00c3d9516d6dc26c243a141d7952298419f
Ardee Aram <ardee.aram@gmail.com>
2023-01-15 23:22:40 +0800

    Add to next.config.js to expose env variables to Lambda functions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  env: {
+    MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID,
+    MY_AWS_ACCESS_SECRET: process.env.MY_AWS_ACCESS_SECRET,
+    MY_AWS_REGION: process.env.MY_AWS_REGION,
+    MY_AWS_USER_POOL_ID: process.env.MY_AWS_USER_POOL_ID
+  }
 }
 
 module.exports = nextConfig
